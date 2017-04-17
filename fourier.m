11186820210b334b44789cb41ed79aa6cc58f381
Osoldier <thomas.ibanez33@gmail.com>
2017-04-17 19:24:23 +0200

    added ifft

--- fourier.m
+++ fourier.m
@@ -4,19 +4,32 @@ f = @(t) cos(2*pi*t)+0.9*cos(20*pi*t);
 period = 0.025;
 t = 0:period:10;
 freq = 1/period;
-sampleSize = size(t);
-sampleSize = sampleSize(2);
+sampleSize = size(t, 2);
 
+%%%%%%%%%%%%%%%% FFT %%%%%%%%%%%%%%%%%
 samples = f(t);
 fhat = fft(samples);
 
-diracs = abs(fhat/sampleSize);
-points = diracs(1:floor(sampleSize/2)+1);
-points(2:end-1) = 2*points(2:end-1);
-
+%fhat has to be normalized by the sample size (see instructions)
+module = abs(fhat/sampleSize);
+%only keep the positive spectrum
+halfTransform = module(1:floor(sampleSize/2)+1);
+%Since power was divided with the negative spectrum, we double the values
+halfTransform(1:end-1) = 2*halfTransform(1:end-1);
+%frequency axis
 a = freq*(0:(sampleSize/2))/sampleSize;
+figure(1);
+plot(a, halfTransform);
+title('Fourier Transform of f(t)');
+xlabel('f (Hz)');
+ylabel('fhat(w)');
 
-plot(a,points) 
-title('Fourier Transform of f(t)')
-xlabel('f (Hz)')
-ylabel('f(w)')
+%%%%%%%%%%%%%%%% IFFT %%%%%%%%%%%%%%%%%
+f2 = ifft(fhat, 'symmetric');
+figure(2);
+plot(t(1:size(f2, 2)), f(t(1:size(f2, 2))), 'b');
+hold on;
+plot(t(1:size(f2, 2)), f2, 'r'); 
+title('Inverse Fourier Transform of fhat(w)');
+xlabel('t (s)');
+ylabel('f(t)');
