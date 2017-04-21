f72cd3e54c9bb25a70922abc200171f91bfffe17
Osoldier <thomas.ibanez33@gmail.com>
2017-04-21 23:32:14 +0200

    fixed

--- fourier.m
+++ fourier.m
@@ -1,5 +1,5 @@
 clear
-f = @(t) cos(2*pi*t)+0.2*cos(20*pi*t);
+f = @(t) cos(2*pi*t)+0.9*cos(20*pi*t);
 
 lowDirac = 1; %Hz
 highDirac = 10; %Hz
@@ -12,14 +12,14 @@ sampleSize = size(t, 2);
 
 %%%%%%%%%%%%%%%% FFT %%%%%%%%%%%%%%%%%
 samples = f(t);
-fhat = fft(samples);
+fhat = fftshift(fft(samples));
 
 %fhat has to be normalized by the sample size (see instructions)
 module = abs(fhat/sampleSize);
 %only keep the positive spectrum
-halfTransform = module(1:floor(sampleSize/2)+1);
+halfTransform = module(ceil(sampleSize/2):end);
 %Since power was divided with the negative spectrum, we double the values
-halfTransform(1:end-1) = 2*halfTransform(1:end-1);
+halfTransform(1:end) = 2*halfTransform(1:end);
 %frequency axis (btw respects shannon sampling theorem) (0->f/2)
 a = freq*(0:(sampleSize/2))/sampleSize;
 figure(1);
@@ -29,7 +29,7 @@ xlabel('f (Hz)');
 ylabel('fhat(w)');
 
 %%%%%%%%%%%%%%%% IFFT %%%%%%%%%%%%%%%%%
-f2 = ifft(fhat, 'symmetric');
+f2 = ifft(ifftshift(fhat), 'symmetric');
 figure(2);
 fplot(f, [0 5]);
 hold on;
@@ -39,7 +39,7 @@ xlabel('t (s)');
 ylabel('f(t)');
 
 %%%%%%%%%%%%%% IFFT 2 %%%%%%%%%%%%%%%%%%%
-fhatlow = filterFFT(fhat, 0, lowDirac+region, freq);
+fhatlow = filterFFT(halfTransform, 0, lowDirac+region, freq)*sampleSize/4;
 figure(3);
 plot(fhatlow);
 title('Filtered Fourier Transform of f(t)');
@@ -53,7 +53,7 @@ hold on;
 plot(t(1:size(f3, 2)), f3, 'r'); 
 
 %%%%%%%%%%%%%% IFFT 3 %%%%%%%%%%%%%%%%%%%
-fhathigh = filterFFT(fhat, highDirac-region, highDirac+region, freq);
+fhathigh = filterFFT(halfTransform, highDirac-region, highDirac+region, freq)*sampleSize/4;
 figure(5);
 plot(fhathigh);
 title('Filtered Fourier Transform of f(t)');
@@ -74,3 +74,24 @@ fclose(fileID);
 figure(7);
 plot(h(1,:), h(2,:));
 %%%%% FFT 2 %%%%%%
+
+fhat = fftshift(fft(h(2,:)));
+sampleSize = size(fhat,2);
+
+module = abs(fhat/sampleSize);
+halfTransform = module(ceil(sampleSize/2):end);
+halfTransform(1:end) = 2*halfTransform(1:end);
+a = freq*(0:(sampleSize/2))/sampleSize;
+
+figure(8);
+plot(a, halfTransform);
+title('Fourier Transform of h(t)');
+xlabel('f (Hz)');
+ylabel('hhat(w)');
+
+fhatfiltered = filterFFT(halfTransform, 0, 10, freq);
+figure(9);
+plot(a, fhatfiltered);
+title('Filtered Fourier Transform of h(t)');
+xlabel('f (Hz)');
+ylabel('hhat(w)');
